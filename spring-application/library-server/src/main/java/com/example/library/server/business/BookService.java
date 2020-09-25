06315d5fb2fc109935efa1e76205beaaa7a12831
tiliavir <tiliavir@gmail.com>
2020-09-25 16:24:49 +0200

    fixes BookService in Server component (partially)

--- spring-application/library-server/src/main/java/com/example/library/server/business/BookService.java
+++ spring-application/library-server/src/main/java/com/example/library/server/business/BookService.java
@@ -3,6 +3,7 @@ package com.example.library.server.business;
 import ch.baloise.keycloak.client.admin.api.Book;
 import ch.baloise.keycloak.client.admin.api.User;
 import com.example.library.server.api.resource.BookResource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -20,106 +22,130 @@ import java.util.UUID;
 @Transactional(readOnly = true)
 public class BookService {
 
-    @Value("library.inventory-srv")
-    private String bookServiceUri;
+  @Value("library.inventory-srv")
+  private String bookServiceUri;
 
-    @Transactional
-    @PreAuthorize("hasRole('LIBRARY_CURATOR')")
-    public UUID create(Book book) {
-        RestTemplate restTemplate = new RestTemplateBuilder()
-                // todo auth
-                .rootUri(bookServiceUri)
-                .build();
-        ResponseEntity<BookResource> response = restTemplate.postForEntity("", book, BookResource.class);
-        return response.getBody() == null ? null : response.getBody().getIdentifier();
+  UserService userService;
+
+  @Value("library.borrow-srv")
+  private String borrowServiceUri;
+
+  @Autowired
+  public BookService(UserService userService) {
+    this.userService = userService;
+  }
+
+  @Transactional
+  @PreAuthorize("hasRole('LIBRARY_CURATOR')")
+  public UUID create(Book book) {
+    RestTemplate restTemplate = new RestTemplateBuilder()
+                                    // todo auth
+                                    .rootUri(bookServiceUri)
+                                    .build();
+    ResponseEntity<BookResource> response = restTemplate.postForEntity("", book, BookResource.class);
+    return response.getBody() == null ? null : response.getBody().getIdentifier();
+  }
+
+  @Transactional
+  @PreAuthorize("hasRole('LIBRARY_CURATOR')")
+  public UUID update(Book book) {
+    RestTemplate restTemplate = new RestTemplateBuilder()
+                                    // todo auth
+                                    .rootUri(bookServiceUri)
+                                    .build();
+    restTemplate.put(book.getIdentifier().toString(), book);
+    return book.getIdentifier();
+  }
+
+  @PreAuthorize("isAuthenticated()")
+  public Optional<Book> findByIdentifier(UUID uuid) {
+    return findWithDetailsByIdentifier(uuid);
+  }
+
+  @PreAuthorize("isAuthenticated()")
+  public Optional<Book> findWithDetailsByIdentifier(UUID uuid) {
+    RestTemplate restTemplate = new RestTemplateBuilder()
+                                    // todo auth
+                                    .rootUri(bookServiceUri)
+                                    .build();
+    ResponseEntity<Book> response = restTemplate.getForEntity(uuid.toString(), Book.class);
+    return response.getBody() == null ? Optional.empty() : Optional.of(response.getBody());
+  }
+
+  @Transactional
+  @PreAuthorize("hasRole('LIBRARY_USER')")
+  public void borrowById(UUID bookIdentifier, UUID userIdentifier) {
+    if (bookIdentifier == null || userIdentifier == null) {
+      return;
     }
 
-    @Transactional
-    @PreAuthorize("hasRole('LIBRARY_CURATOR')")
-    public UUID update(Book book) {
-        return bookRepository.save(book).getIdentifier();
-    }
-
-    @PreAuthorize("isAuthenticated()")
-    public Optional<Book> findByIdentifier(UUID uuid) {
-        return bookRepository.findOneByIdentifier(uuid);
-    }
-
-    @PreAuthorize("isAuthenticated()")
-    public Optional<Book> findWithDetailsByIdentifier(UUID uuid) {
-        return bookRepository.findOneWithDetailsByIdentifier(uuid);
-    }
-
-    @Transactional
-    @PreAuthorize("hasRole('LIBRARY_USER')")
-    public void borrowById(UUID bookIdentifier, UUID userIdentifier) {
-
-        if (bookIdentifier == null || userIdentifier == null) {
-            return;
-        }
-
-        userRepository
-                .findOneByIdentifier(userIdentifier)
+    userService.findByIdentifier(userIdentifier).ifPresent(
+        u ->
+            this.findByIdentifier(bookIdentifier)
                 .ifPresent(
-                        u ->
-                                bookRepository
-                                        .findOneByIdentifier(bookIdentifier)
-                                        .ifPresent(
-                                                b -> {
-                                                    doBorrow(b, u);
-                                                    bookRepository.save(b);
-                                                }));
+                    b -> {
+                      doBorrow(b, u);
+                      this.update(b);
+                    }));
+  }
+
+  @Transactional
+  @PreAuthorize("hasRole('LIBRARY_USER')")
+  public void returnById(UUID bookIdentifier, UUID userIdentifier) {
+
+    if (bookIdentifier == null || userIdentifier == null) {
+      return;
     }
 
-    @Transactional
-    @PreAuthorize("hasRole('LIBRARY_USER')")
-    public void returnById(UUID bookIdentifier, UUID userIdentifier) {
+    userService
+        .findByIdentifier(userIdentifier)
+        .ifPresent(
+            u ->
+                findByIdentifier(bookIdentifier)
+                    .ifPresent(
+                        b -> {
+                          doReturn(b, u);
+                          update(b);
+                        }));
+  }
 
-        if (bookIdentifier == null || userIdentifier == null) {
-            return;
-        }
+  @PreAuthorize("isAuthenticated()")
+  public List<Book> findAll() {
+    RestTemplate restTemplate = new RestTemplateBuilder()
+                                    // todo auth
+                                    .rootUri(bookServiceUri)
+                                    .build();
+    ResponseEntity<Book[]> response = restTemplate.getForEntity("", Book[].class);
+    return response.getBody() == null ? null : Arrays.asList(response.getBody());
+  }
 
-        userRepository
-                .findOneByIdentifier(userIdentifier)
-                .ifPresent(
-                        u ->
-                                bookRepository
-                                        .findOneByIdentifier(bookIdentifier)
-                                        .ifPresent(
-                                                b -> {
-                                                    doReturn(b, u);
-                                                    bookRepository.save(b);
-                                                }));
+  @Transactional
+  @PreAuthorize("hasRole('LIBRARY_CURATOR')")
+  public void deleteByIdentifier(UUID bookIdentifier) {
+    RestTemplate restTemplate = new RestTemplateBuilder()
+                                    // todo auth
+                                    .rootUri(bookServiceUri)
+                                    .build();
+    restTemplate.delete(bookIdentifier.toString());
+  }
+
+  private void doReturn(Book book, User user) {
+    if (book.isBorrowed()) {
+      if (book.getBorrowedBy().equals(user)) {
+        book.setBorrowed(false);
+        book.setBorrowedBy(null);
+      } else {
+        throw new AccessDeniedException(
+            String.format(
+                "User %s cannot return a book borrowed by another user", user.getEmail()));
+      }
     }
+  }
 
-    @PreAuthorize("isAuthenticated()")
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+  private void doBorrow(Book book, User user) {
+    if (!book.isBorrowed()) {
+      book.setBorrowed(true);
+      book.setBorrowedBy(user);
     }
-
-    @Transactional
-    @PreAuthorize("hasRole('LIBRARY_CURATOR')")
-    public void deleteByIdentifier(UUID bookIdentifier) {
-        bookRepository.deleteBookByIdentifier(bookIdentifier);
-    }
-
-    private void doReturn(Book book, User user) {
-        if (book.isBorrowed()) {
-            if (book.getBorrowedBy().equals(user)) {
-                book.setBorrowed(false);
-                book.setBorrowedBy(null);
-            } else {
-                throw new AccessDeniedException(
-                        String.format(
-                                "User %s cannot return a book borrowed by another user", user.getEmail()));
-            }
-        }
-    }
-
-    private void doBorrow(Book book, User user) {
-        if (!book.isBorrowed()) {
-            book.setBorrowed(true);
-            book.setBorrowedBy(user);
-        }
-    }
+  }
 }
