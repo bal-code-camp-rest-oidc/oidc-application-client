4110340f3461053a433fe5ba9e902bd30494f6b5
Roland Seidel <register@interpolar.ch>
2020-10-11 23:48:18 +0200

    Add AudienceValidater and jwtDecoder to all secured services

--- spring-application/library-borrow-service/src/main/java/com/example/library/borrow/config/WebSecurityConfiguration.java
+++ spring-application/library-borrow-service/src/main/java/com/example/library/borrow/config/WebSecurityConfiguration.java
@@ -1,6 +1,9 @@
 package com.example.library.borrow.config;
 
 import com.example.library.borrow.properties.swagger.SwaggerKeycloakProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,6 +12,12 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -24,6 +33,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+  private final OAuth2ResourceServerProperties oAuth2ResourceServerProperties;
 
   /**
    * Provides the properties we use to access keycloak using the correct client infos.
@@ -35,8 +45,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
    *
    * @param swaggerKeycloakProperties properties we use to access keycloak using the correct client infos.
    */
-  public WebSecurityConfiguration(SwaggerKeycloakProperties swaggerKeycloakProperties) {
+  public WebSecurityConfiguration(SwaggerKeycloakProperties swaggerKeycloakProperties,
+                                  OAuth2ResourceServerProperties oAuth2ResourceServerProperties) {
     this.swaggerKeycloakProperties = swaggerKeycloakProperties;
+    this.oAuth2ResourceServerProperties = oAuth2ResourceServerProperties;
   }
 
   @Bean
@@ -66,23 +78,39 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-        .cors(withDefaults())
-        .csrf()
-        .disable()
-        .authorizeRequests()
-        .anyRequest()
-        .fullyAuthenticated()
-        .and()
-        .oauth2ResourceServer()
-        .jwt();
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .cors(withDefaults()).csrf().disable()
+            .authorizeRequests()
+            .anyRequest()
+            .fullyAuthenticated()
+            .and()
+            .oauth2ResourceServer()
+            .jwt();
+  }
+
+  @Bean
+  JwtDecoder jwtDecoder() {
+    NimbusJwtDecoder jwtDecoder =
+            NimbusJwtDecoder.withJwkSetUri(oAuth2ResourceServerProperties.getJwt().getJwkSetUri())
+                    .build();
+
+    OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator();
+    OAuth2TokenValidator<Jwt> withIssuer =
+            JwtValidators.createDefaultWithIssuer(
+                    oAuth2ResourceServerProperties.getJwt().getIssuerUri());
+    OAuth2TokenValidator<Jwt> withAudience =
+            new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+
+    jwtDecoder.setJwtValidator(withAudience);
+
+    return jwtDecoder;
   }
 
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("https://localhost:9090", "http://localhost:9090"));
+    configuration.setAllowedOrigins(Arrays.asList("*", "https://localhost:9090", "http://localhost:9090"));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
     configuration.setAllowedHeaders(Collections.singletonList(CorsConfiguration.ALL));
     configuration.setAllowCredentials(true);
